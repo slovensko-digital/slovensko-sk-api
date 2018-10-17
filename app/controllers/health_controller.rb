46366de86a77a0beb7e3de397bb6feee499df6f8
Pavol Zbell <pavol.zbell@gmail.com>
2018-10-17 18:30:16 +0200

    Add health checks

---
+++ app/controllers/health_controller.rb
@@ -0,0 +1,5 @@
+class HealthController < ApplicationController
+  def index
+    render status: :ok
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,3 @@
 Rails.application.routes.draw do
+  get :health, to: 'health#index'
 end
