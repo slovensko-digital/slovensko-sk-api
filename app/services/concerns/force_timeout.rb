7829921429aa479300f0c117418a6d2c553d6323
Pavol Zbell <pavol.zbell@gmail.com>
2018-10-23 10:36:28 +0200

    Add notes

--- app/services/concerns/force_timeout.rb
+++ app/services/concerns/force_timeout.rb
@@ -1,3 +1,5 @@
+# TODO remove this, try to force timeout via http://cxf.apache.org/docs/client-http-transport-including-ssl-support.html#ClientHTTPTransport(includingSSLsupport)-Theclientelement
+
 module ForceTimeout
   extend ActiveSupport::Concern
 
