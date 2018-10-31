7db739f08c23c410855b5cde91e8597a0a167915
Pavol Zbell <pavol.zbell@gmail.com>
2018-10-31 23:52:43 +0100

    Favor composition

--- app/services/sktalk_receiver.rb
+++ app/services/sktalk_receiver.rb
@@ -1,6 +1,15 @@
-class SktalkReceiver < UpvsProxy
-  def receive(message, wait: 60.seconds)
-    message = digital.slovensko.upvs.SktalkMessages::from_xml(message)
-    timeout(wait) { upvs.sktalk.receive(message) }
+# TODO try to force timeout via http://cxf.apache.org/docs/client-http-transport-including-ssl-support.html#ClientHTTPTransport(includingSSLsupport)-Theclientelement
+
+class SktalkReceiver
+  include ForceTimeout
+  include UnwrapErrors
+
+  def initialize(proxy)
+    @upvs = proxy
+  end
+
+  def receive(message)
+    message = SktalkMessages.from_xml(message)
+    timeout(60.seconds) { @upvs.sktalk.receive(message) }
   end
 end
--- app/services/upvs_proxy.rb
+++ app/services/upvs_proxy.rb
@@ -1,13 +1,6 @@
 class UpvsProxy
-  include ForceTimeout
-  include UnwrapErrors
-
-  attr_reader :upvs
-
   def initialize(properties)
     # TODO cache Upvs objects here - consider Java vs Ruby cache
     @upvs = digital.slovensko.upvs.UpvsProxy.new(properties)
   end
-
-  private :upvs
 end
