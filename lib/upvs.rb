d63282144075ea83ed7dd8daddaee467af6fb1f1
Pavol Zbell <pavol.zbell@gmail.com>
2018-11-01 01:28:35 +0100

    Clean up

--- lib/upvs.rb
+++ lib/upvs.rb
@@ -1,11 +1,5 @@
-# require Java libraries
-
 root = File.expand_path('upvs', __dir__)
 
 Dir[File.join(root, 'bin', 'lib', '*.jar')].each { |jar| require jar }
 
 require File.join(root, 'bin', 'upvs-0.0.0.jar')
-
-# alter Java classes for Ruby usage
-
-# TODO
