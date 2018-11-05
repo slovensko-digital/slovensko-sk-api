08088bbf2972373f0e93a1bb1e4bb15d75aee606
Pavol Zbell <pavol.zbell@gmail.com>
2018-11-05 13:14:47 +0100

    Add Java to Ruby object structure transformation helper

---
+++ app/helpers/upvs_objects.rb
@@ -0,0 +1,21 @@
+module UpvsObjects
+  extend self
+
+  def to_structure(object)
+    to_ruby_object(digital.slovensko.upvs.UpvsObjects.to_structure(object))
+  end
+
+  private
+
+  def to_ruby_object(object)
+    return object.to_a.map { |e| to_ruby_object(e) } if object.is_a?(java.util.Collection)
+    return object.to_h.map { |k, v| [to_ruby_key(k), to_ruby_object(v)]}.to_h if object.is_a?(java.util.Map)
+    object
+  end
+
+  def to_ruby_key(key)
+    return to_ruby_object(key) unless key.is_a?(String)
+    fix = { 'clazz' => 'class', 'e_desk' => 'edesk', 'sk_talk' => 'sktalk' }
+    fix.inject(key.underscore) { |k, (p, r)| k.sub(/(\A|_)#{p}(_|\z)/i, r) }
+  end
+end
