a0a2d1d7c204fdae4cfbcb8deb83ed0eb77dec66
Pavol Zbell <pavol.zbell@gmail.com>
2018-11-06 15:51:37 +0100

    Switch to fix

--- app/services/upvs_environment.rb
+++ app/services/upvs_environment.rb
@@ -13,21 +13,28 @@ module UpvsEnvironment
     # TODO
 
     {
-      'upvs.eks.address' => 'https://edeskii.vyvoj.upvs.globaltel.sk/EKSService.svc',
-      'upvs.ez.address' => 'https://vyvoj.upvs.globaltel.sk/ServiceBus/ServiceBusToken.svc',
-      'upvs.iam.address' => 'https://authws.vyvoj.upvs.globaltel.sk/iamws17/GetIdentityService',
-      'upvs.sktalk.address' => 'https://vyvoj.upvs.globaltel.sk/g2g/G2GServiceBus/ServiceSkTalk3Token.svc',
-      'upvs.sts.address' => 'https://authws.vyvoj.upvs.globaltel.sk/sts/wss11x509',
+      # 'upvs.eks.address' => 'https://edeskii.vyvoj.upvs.globaltel.sk/EKSService.svc',
+      # 'upvs.ez.address' => 'https://vyvoj.upvs.globaltel.sk/ServiceBus/ServiceBusToken.svc',
+      # 'upvs.iam.address' => 'https://authws.vyvoj.upvs.globaltel.sk/iamws17/GetIdentityService',
+      # 'upvs.sktalk.address' => 'https://vyvoj.upvs.globaltel.sk/g2g/G2GServiceBus/ServiceSkTalk3Token.svc',
+      # 'upvs.sts.address' => 'https://authws.vyvoj.upvs.globaltel.sk/sts/wss11x509',
 
-      'upvs.log.file.pattern' => 'log/upvsdev-%d{yyyyMMdd}.log',
+      'upvs.eks.address' => 'https://eschranka.upvsfixnew.gov.sk/EKSService.svc',
+      'upvs.ez.address' => 'https://usr.upvsfixnew.gov.sk/ServiceBus/ServiceBusToken.svc',
+      'upvs.iam.address' => '',
+      'upvs.sktalk.address' => 'https://uir.upvsfixnew.gov.sk/G2GServiceBus/ServiceSkTalk3Token.svc',
+      'upvs.sts.address' => 'https://iamwse.upvsfix.gov.sk:8581/sts/wss11x509',
+
+      'upvs.log.console' => 'OFF',
+      'upvs.log.file.pattern' => 'log/upvs-%d{yyyyMMdd}.log',
       'upvs.log.java.console.level' => 'INFO',
 
-      'upvs.ssl.truststore.file' => 'tmp/upvsdev.truststore',
+      'upvs.ssl.truststore.file' => 'tmp/security/upvs-fix.truststore',
       'upvs.ssl.truststore.type' => 'JKS',
       'upvs.ssl.truststore.password' => ENV['UPVS_TS_PASS'],
 
-      'upvs.crypto.keystore.file' => 'tmp/irvin_upvsdev.keystore',
-      'upvs.crypto.keystore.alias' => 'irvin_upvsdev',
+      'upvs.crypto.keystore.file' => 'tmp/security/irvin-fix.keystore',
+      'upvs.crypto.keystore.alias' => 'irvin_upvsfix',
       'upvs.crypto.keystore.type' => 'JKS',
       'upvs.crypto.keystore.password' => ENV['UPVS_KS_PASS'],
       'upvs.crypto.keystore.key' => ENV['UPVS_KS_KEY'],
