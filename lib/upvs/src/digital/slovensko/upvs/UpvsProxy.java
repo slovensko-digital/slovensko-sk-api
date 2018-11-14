45206552f8ce663c9221505eaebc3215a9991fc0
Ernest Walzel <ewalzel@gmail.com>
2018-11-14 16:13:36 +0100

    Replace EzClient by ruby code

--- lib/upvs/src/digital/slovensko/upvs/UpvsProxy.java
+++ lib/upvs/src/digital/slovensko/upvs/UpvsProxy.java
@@ -3,7 +3,6 @@ package digital.slovensko.upvs;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import digital.slovensko.upvs.client.EzClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.env.MapPropertySource;
@@ -16,13 +15,14 @@ import sk.gov.schemas.edesk.eksservice._1.IEKSService;
 import sk.gov.schemas.identity.service._1_7.IdentityServices;
 
 import digital.slovensko.upvs.log.PropertyResolver;
+import sk.gov.schemas.servicebus.service._1_0.IServiceBus;
 
 public final class UpvsProxy {
   private final ApplicationContext context;
   private final IEKSService eks;
   private final IdentityServices iam;
   private final IService sktalk;
-  private final EzClient ezClient;
+  private final IServiceBus serviceBus;
 
   public UpvsProxy(final Map<String, String> properties) {
     this.context = new Context(new MapPropertySource("upvs", new LinkedHashMap<>(properties)));
@@ -30,7 +30,7 @@ public final class UpvsProxy {
     this.eks = this.context.getBean(IEKSService.class);
     this.iam = this.context.getBean(IdentityServices.class);
     this.sktalk = this.context.getBean(IService.class);
-    this.ezClient = this.context.getBean(EzClient.class);
+    this.serviceBus = this.context.getBean(IServiceBus.class);
 
     PropertyResolver.load(properties); // TODO remove
   }
@@ -63,7 +63,7 @@ public final class UpvsProxy {
     return this.sktalk;
   }
 
-  public EzClient getEzClient() {
-    return this.ezClient;
+  public IServiceBus getServiceBus() {
+    return this.serviceBus;
   }
 }
