c8455c3a38f21055e15705f82ecafccf0b9bd2ef
Ernest Walzel <ewalzel@gmail.com>
2018-11-14 14:44:28 +0100

    examples/eform_find_form_templates.rb working

--- app/examples/eform_find_form_templates.rb
+++ app/examples/eform_find_form_templates.rb
@@ -1,4 +1,8 @@
 require_relative '../../config/environment'
 
-eform = UpvsEnvironment.upvs_proxy(nil).eform
-eform.call_service(sk.gov.schemas.servicebus.service._1.ServiceClassEnum::EFORM_FINDFORMTEMPLATES_SOAP_V_1_0, nil)
+ez_client = UpvsEnvironment.upvs_proxy(nil).ez_client
+form_templates = ez_client.find_form_templates.get_form_templates.get_value.get_form_template_id
+
+form_templates.each do |ft|
+  puts "#{ft.get_identifier.get_value} #{ft.get_version.get_value.get_major}.#{ft.get_version.get_value.get_minor}"
+end
