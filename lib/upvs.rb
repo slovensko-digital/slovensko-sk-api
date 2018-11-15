517436e62df112eb292af02766fe4335d72c4564
Pavol Zbell <pavol.zbell@gmail.com>
2018-11-15 14:53:09 +0100

    Do not compile UPVS on load error

--- lib/upvs.rb
+++ lib/upvs.rb
@@ -2,10 +2,4 @@ root = File.expand_path('upvs', __dir__)
 
 Dir[File.join(root, 'bin', 'lib', '*.jar')].each { |jar| require jar }
 
-# TODO clean up this
-begin
-  require File.join(root, 'bin', 'upvs-0.0.0.jar')
-rescue LoadError
-  `lib/upvs/compile`
-  require File.join(root, 'bin', 'upvs-0.0.0.jar')
-end
+require File.join(root, 'bin', 'upvs-0.0.0.jar')
