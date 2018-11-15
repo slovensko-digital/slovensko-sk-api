5b9d605fe12943c8d5b194b40a3b55d0879476ca
Pavol Zbell <pavol.zbell@gmail.com>
2018-11-15 18:07:16 +0100

    Try to compile within the same environment

--- lib/upvs.rb
+++ lib/upvs.rb
@@ -1,7 +1,7 @@
 root = File.expand_path('upvs', __dir__)
 main = File.join(root, 'bin', 'upvs-0.0.0.jar')
 
-system File.join(root, 'compile') unless File.exists?(main)
+system(ENV, File.join(root, 'compile')) unless File.exists?(main)
 
 Dir[File.join(root, 'bin', 'lib', '*.jar')].each { |jar| require jar }
 
