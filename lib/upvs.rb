96a6531204c5a736284f4628a60f6e6bd8e319a3
Pavol Zbell <pavol.zbell@gmail.com>
2018-11-15 17:12:30 +0100

    Compile UPVS library on demand again

--- lib/upvs.rb
+++ lib/upvs.rb
@@ -1,5 +1,7 @@
 root = File.expand_path('upvs', __dir__)
 
+system File.join(root, 'compile') if Dir.empty? File.join(root, 'bin')
+
 Dir[File.join(root, 'bin', 'lib', '*.jar')].each { |jar| require jar }
 
 require File.join(root, 'bin', 'upvs-0.0.0.jar')
