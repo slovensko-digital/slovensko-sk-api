1a2e513e79066cf869d05c01f8918925ea7a013b
Pavol Zbell <pavol.zbell@gmail.com>
2018-11-19 12:51:08 +0100

    Read SP metadata from XML

--- config/initializers/upvs_omniauth.rb
+++ config/initializers/upvs_omniauth.rb
@@ -7,6 +7,12 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     config.logger = Rails.logger
   end
 
+  # Read identity provider metadata
+  idp_metadata = OneLogin::RubySaml::IdpMetadataParser.new.parse_to_hash(File.read(ENV.fetch('UPVS_IDP_METADATA')))
+
+  # Read service provider metadata
+  sp_metadata = Hash.from_xml(File.read(ENV.fetch('UPVS_SP_METADATA'))).fetch('EntityDescriptor')
+
   begin
     # Read service provider keystore
     keystore = java.security.KeyStore.get_instance('JKS')
@@ -18,23 +24,16 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     raise "#{e.class}: #{e.message}" # TODO
   end
 
-  # Read identity provider metadata
-  idp_metadata = OneLogin::RubySaml::IdpMetadataParser.new.parse_to_hash(File.read(ENV.fetch('UPVS_IDP_METADATA')))
-
-  # Read server setup
-  protocol = Rails.application.config.force_ssl ? 'https' : 'http'
-  host, port = Rails::Server::Options.new.parse!(ARGV).slice(:Host, :Port).values
-
   # Assemble settings
   settings = idp_metadata.merge(
     request_path: '/auth/saml/login',
     callback_path: '/auth/saml/callback',
 
-    issuer: ENV.fetch('UPVS_SP_ISSUER'),
-    assertion_consumer_service_url: "#{protocol}://#{host}:#{port}/auth/saml/callback",
+    issuer: sp_metadata['entityID'],
+    assertion_consumer_service_url: sp_metadata['SPSSODescriptor']['AssertionConsumerService'].first['Location'],
     name_identifier_format: 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
     protocol_binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-    sp_name_qualifier: ENV.fetch('UPVS_SP_ISSUER'),
+    sp_name_qualifier: sp_metadata['entityID'],
 
     certificate: certificate,
     private_key: private_key,
