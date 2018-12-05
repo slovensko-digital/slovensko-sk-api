bea5f11205a87dadb8d1968bb105ae5ed89d244d
Pavol Zbell <pavol.zbell@gmail.com>
2018-12-05 20:42:32 +0100

    Allow specs to decide whether enable auth test mode or not

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-# Enable test mode in test environment.
-OmniAuth.config.test_mode = true if Rails.env.test?
-
 # Raise errors in every environment instead of redirecting to the default error page.
 OmniAuth.config.failure_raise_out_environments = ['development', 'production', 'staging', 'test']
