7adaf4a7740e66091cdb244622999c2d81289c4d
Ernest Walzel <ewalzel@gmail.com>
2018-12-13 17:29:49 +0100

    Verify auth token

--- app/controllers/eform_controller.rb
+++ app/controllers/eform_controller.rb
@@ -1,4 +1,6 @@
 class EformController < ApiController
+  before_action :require_token
+
   def validate
     version_major, version_minor = params[:version].split('.')
     form_template = FormTemplate.find_by!(identifier: params[:identifier], version_major: version_major, version_minor: version_minor)
@@ -10,5 +12,12 @@ class EformController < ApiController
     render json: { valid: validation_errors.empty?, errors: validation_errors.map(&:message) }
   rescue Nokogiri::XML::SyntaxError
     render json: { valid: false, errors: ['Malformed XML'] }
+  end
+
+  private
+
+  def require_token
+    render_bad_request('No credentials') and return if params[:token].blank?
+    authenticator.verify_token(params[:token])
   end
 end
--- spec/requests/api/eform_spec.rb
+++ spec/requests/api/eform_spec.rb
@@ -1,23 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'eForm API' do
-  before(:each) do
-    create(:form_template_related_document, :general_agenda_xsd_schema)
-  end
-
-  let(:response_object) { JSON.parse(response.body) }
-
-  valid_form_xml = <<~HEREDOC
-    <GeneralAgenda xmlns="http://schemas.gov.sk/form/App.GeneralAgenda/1.7" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-      <subject>some subject</subject>
-      <text>some text</text>
-    </GeneralAgenda>
-  HEREDOC
-
   describe 'POST /api/eform/validate' do
+    valid_form_xml = <<~HEREDOC
+      <GeneralAgenda xmlns="http://schemas.gov.sk/form/App.GeneralAgenda/1.7" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <subject>some subject</subject>
+        <text>some text</text>
+      </GeneralAgenda>
+    HEREDOC
+    default_params = { identifier: 'App.GeneralAgenda', version: '1.7', data: valid_form_xml, token: 'TEST_TOKEN' }
+
+    let(:response_object) { JSON.parse(response.body) }
+
+    before(:each) do
+      create(:form_template_related_document, :general_agenda_xsd_schema)
+      allow(ApiEnvironment.token_authenticator).to receive(:verify_token).with('TEST_TOKEN')
+    end
+
+    def post_request(params)
+      post '/api/eform/validate', params: params
+    end
+
+    describe 'auth token' do
+      it 'is required' do
+        expect(ApiEnvironment.token_authenticator).to receive(:verify_token).with('invalid').and_call_original
+        post_request default_params.merge({ token: 'invalid' })
+        expect(response.status).to eq(401)
+      end
+
+      pending 'does not need obo token'
+    end
+
     describe 'when form data is valid' do
       it 'returns validation result' do
-        post '/api/eform/validate', params: { identifier: 'App.GeneralAgenda', version: '1.7', data: valid_form_xml }
+        post_request default_params
         expect(response_object['valid']).to eq(true)
         expect(response_object['errors']).to be_blank
       end
@@ -30,7 +46,7 @@ RSpec.describe 'eForm API' do
       HEREDOC
 
       it 'lists validation errors' do
-        post '/api/eform/validate', params: { identifier: 'App.GeneralAgenda', version: '1.7', data: invalid_form_xml }
+        post_request default_params.merge({ data: invalid_form_xml })
         expect(response_object['valid']).to eq(false)
         expect(response_object['errors']).to be_present
       end
@@ -48,7 +64,7 @@ RSpec.describe 'eForm API' do
       unparseable = 'Not an XML.'
 
       it 'the form is deemed invalid' do
-        post '/api/eform/validate', params: { identifier: 'App.GeneralAgenda', version: '1.7', data: unparseable }
+        post_request default_params.merge({ data: unparseable })
         expect(response_object['valid']).to eq(false)
         expect(response_object['errors']).to include('Malformed XML')
       end
