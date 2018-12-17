c0efeafca2b3d359bc7d9621119ee78358136f14
Pavol Zbell <pavol.zbell@gmail.com>
2018-12-17 11:35:54 +0100

    Space

--- app/controllers/eform_controller.rb
+++ app/controllers/eform_controller.rb
@@ -6,7 +6,7 @@ class EformController < ApiController
     form_template = FormTemplate.find_by!(identifier: params[:identifier], version_major: version_major, version_minor: version_minor)
 
     xsd_schema = Nokogiri::XML::Schema(form_template.xsd_schema)
-    form_xml = Nokogiri::XML(params[:data]){ |config| config.strict }
+    form_xml = Nokogiri::XML(params[:data]) { |config| config.strict }
 
     validation_errors = xsd_schema.validate(form_xml)
     render json: { valid: validation_errors.empty?, errors: validation_errors.map(&:message) }
