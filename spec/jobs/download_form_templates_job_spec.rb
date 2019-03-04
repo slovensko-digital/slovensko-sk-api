f2a3e010504500271e213151b07614b07aaa3aad
Pavol Zbell <pavol.zbell@gmail.com>
2019-03-04 21:02:20 +0100

    Fix eForm templates downloading job

--- spec/jobs/download_form_templates_job_spec.rb
+++ spec/jobs/download_form_templates_job_spec.rb
@@ -7,7 +7,10 @@ RSpec.describe DownloadFormTemplatesJob, :upvs, type: :job do
     end
 
     before(:example) do
-      expect(eform_service).to receive(:fetch_all_form_template_ids).and_return(@response)
+      eform_service = double
+
+      allow(UpvsEnvironment).to receive(:eform_service).and_return(eform_service)
+      allow(eform_service).to receive(:fetch_all_form_template_ids).and_return(@response)
     end
 
     it 'enqueues DownloadFormTemplateJob for new templates by default' do
@@ -21,6 +24,12 @@ RSpec.describe DownloadFormTemplatesJob, :upvs, type: :job do
     end
 
     it 'enqueues DownloadFormTemplateJob for all templates when forced' do
+      FormTemplate.create(
+        identifier: @response.first.identifier.value,
+        version_major: @response.first.version.value.major,
+        version_minor: @response.first.version.value.minor
+      )
+
       expect { subject.perform(force: true) }.to have_enqueued_job(DownloadFormTemplateJob).exactly(3).times
     end
   end
