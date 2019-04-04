3884dd29ef7ed90a266b6747b553e24906013689
Pavol Zbell <pavol.zbell@gmail.com>
2019-04-04 22:51:19 +0200

    Try CI setups

--- spec/support/upvs_sso_support.rb
+++ spec/support/upvs_sso_support.rb
@@ -1,4 +1,4 @@
-# run all examples with UPVS SSO support enabled by default
+# run all examples with UPVS SSO support enabled by default, i.e. skip all examples with sso tag set to false
 
 RSpec.configure do |config|
   unless (config.inclusion_filter.rules.keys + config.exclusion_filter.rules.keys).include?(:sso)
