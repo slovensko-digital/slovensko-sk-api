afab6498c4b1b38e7e0b3b7b5cb960ccd3f982dc
Pavol Zbell <pavol.zbell@gmail.com>
2020-06-06 16:08:05 +0200

    Use application time zone in clockwork

--- config/clock.rb
+++ config/clock.rb
@@ -5,12 +5,12 @@ Rails.application.load_tasks
 module Clockwork
   configure do |config|
     config[:thread] = true
-    config[:tz] = 'Europe/Bratislava'
+    config[:tz] = Rails.application.config.time_zone
   end
 
-  handler do |job|
-    Rake::Task[job].reenable
-    Rake::Task[job].invoke
+  handler do |name|
+    Rake::Task[name].reenable
+    Rake::Task[name].invoke
   end
 
   every(1.day, 'eform:sync', at: '5:00') if ENV['EFORM_SYNC'] != 'false'
