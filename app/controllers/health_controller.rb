7b1210fe7020fddd5f36fe7a9b0dbbb9b13661a4
luciajanikova <19lucia99@gmail.com>
2022-07-19 12:26:14 +0200

    Release v3.2.0

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -10,7 +10,7 @@ class HealthController < ApiController
     status = :ok
     health = {
       description: 'slovensko.sk API',
-      version: '3.1.2',
+      version: '3.2.0',
       status: 'pass',
       checks: {
         'environment:variables' => environment_variables,
