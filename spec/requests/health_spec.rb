d06c5e381fdc0c38a8fb069d9c2e984410ab946b
luciajanikova <19lucia99@gmail.com>
2024-03-18 15:19:16 +0100

    Release v3.7.2

--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -71,7 +71,7 @@ RSpec.describe 'Health Check' do
       expect(response.status).to eq(200)
       expect(response.object.with_indifferent_access).to match(
         description: 'slovensko.sk API',
-        version: '3.7.1',
+        version: '3.7.2',
         status: 'pass',
         checks: hash_including(*checks),
         links: {
